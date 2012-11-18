7c2cb78af2d001483e127226d270b42d61e9ec20
SHIOYA, Hiromu <kwappa.856@gmail.com>
2012-11-18 09:44:30 +0900

    modify new and confirm screen
     * check extension of uploaded file
      * allow only jpeg image
     * modify confirm screen style
     * remove debug log

--- app/controllers/dishes.rb
+++ app/controllers/dishes.rb
@@ -8,7 +8,6 @@ Meshigoyomi.controllers :dishes do
   post :confirm do
     @page_title = 'あたらしいごはん'
     tupper = prepare_tupper
-    logger.debug params
 
     if params.fetch('photo', []).empty?
       flash[:warning] = '写真がアップロードされていません。'
@@ -17,18 +16,24 @@ Meshigoyomi.controllers :dishes do
 
     begin
       tupper.upload params['photo']
+      if %r{jpe?g}i !~ File.extname(tupper.file_info['uploaded_file'])
+        logger.debug tupper.file_info
+        tupper.cleanup
+        flash[:warning] = "写真はjpegだけですよ"
+        redirect url(:dishes, :new)
+      end
     rescue Tupper::FileSizeError
       tupper.cleanup
       flash[:warning] = "写真のファイルサイズは#{tupper.mas_size}MBまでです。"
       redirect url(:dishes, :new)
     rescue => e
+      tupper.cleanup
       logger.debug e.message
       logger.debug e.backtrace
       halt 500
     end
 
     if tupper.has_uploaded_file?
-      logger.debug tupper.file_info
       preview_url = File.join('/images', 'tupper', File.basename(tupper.file_info['uploaded_file']))
       render :'/dishes/confirm', locals: { preview_url: preview_url }
     else
